27e671a9bf24ba953a64b80eda439ff4507f3545
Brian McLaren <brianqmclaren@gmail.com>
2019-03-08 14:13:23 -0500

    Update python file with mimetypes library

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -2,11 +2,13 @@ import boto3
 from botocore.client import Config
 import StringIO
 import zipfile
+import mimetypes
 
 
 def lambda_handler(event, context):
     sns = boto3.resource('sns')
-    topic = sns.Topic('arn:aws:sns:us-east-1:467851996983:deployPortfolioTopic')
+    topic = sns.Topic(
+        'arn:aws:sns:us-east-1:467851996983:deployPortfolioTopic')
 
     location = {
         'bucketName': 'bqmclaren.build.com',
@@ -34,15 +36,18 @@ def lambda_handler(event, context):
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for nm in myzip.namelist():
                 obj = myzip.open(nm)
-                portfolio_bucket.upload_fileobj(obj, nm)
+                portfolio_bucket.upload_fileobj(
+                    obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
                 portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
 
         print ('Job Done!')
-        topic.publish(Subject='Portfolio Deployed', Message='Portfolio deployed successfully!')
+        topic.publish(Subject='Portfolio Deployed',
+                      Message='Portfolio deployed successfully!')
         if job:
             codepipeline = boto3.client('codepipeline')
             codepipeline.put_job_success_result(jobId=job['id'])
     except:
-        topic.publish(Subject='Portfolio Deployment Failed', Message='The portfolio was not deployed successfully!')
+        topic.publish(Subject='Portfolio Deployment Failed',
+                      Message='The portfolio was not deployed successfully!')
         raise
     return 'Hello from Lambda'
