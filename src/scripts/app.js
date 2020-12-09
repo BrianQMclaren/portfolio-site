9c36b0d32439c44d53cad949e6ba57dc5d46a661
Brian McLaren <brianqmclaren@gmail.com>
2020-12-09 10:42:23 -0500

    Implement contact form, clean up some dead code

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,8 +3,10 @@ import "../bookclub.html";
 import "../realestate.html";
 import "../unibank.html";
 
+import "./modules/emailForm";
 import MobileMenu from "./modules/mobileMenu";
 import RevealOnScroll from "./modules/revealOnScroll";
+
 
 const menu = new MobileMenu();
 const revealScroll = new RevealOnScroll();
